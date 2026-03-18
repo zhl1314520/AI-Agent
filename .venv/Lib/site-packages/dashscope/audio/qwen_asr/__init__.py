07194d0ca640636871f6baa5f9808e35b94c651d
developer <17201665342@163.com>
2026-03-18 11:32:08 +0800

    移除 .idea 和 .venv，添加 .gitignore

--- .venv/Lib/site-packages/dashscope/audio/qwen_asr/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) Alibaba, Inc. and its affiliates.
-
-from .qwen_transcription import QwenTranscription
-
-__all__ = [
-    "QwenTranscription",
-]
--- .venv/Lib/site-packages/dashscope/audio/qwen_tts/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) Alibaba, Inc. and its affiliates.
-
-from .speech_synthesizer import SpeechSynthesizer
-
-__all__ = ["SpeechSynthesizer"]
