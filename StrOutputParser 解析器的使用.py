90d90f08303a2c03e84b39c2c6a02476ddc36309
developer <17201665342@163.com>
2026-03-21 13:03:19 +0800

    了解 chain 和 StrOutPutParser解析器

---
+++ StrOutputParser 解析器的使用.py
@@ -0,0 +1,19 @@
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_community.chat_models.tongyi import ChatTongyi
+from langchain_core.messages import AIMessage
+
+parser = StrOutputParser()
+model = ChatTongyi(model="qwen3-max")
+prompt = PromptTemplate.from_template(
+    "姓：{lastname}，刚生了{gender}。请起名，仅告知我名字无需其它内容。"
+)
+
+"""
+chain = prompt | model | model
+    如果是这样写的话，由于 model 模型的输出是 AIMessage 类型的，不能当作下一个 model 的输入，需要转为字符串
+"""
+chain = prompt | model | parser | model
+
+res: AIMessage = chain.invoke({"lastname": "张", "gender": "女儿"})
+print(res.content)
