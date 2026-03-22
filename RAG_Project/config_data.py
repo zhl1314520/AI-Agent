aa786fc870fbe151cfa221619bac577bcb7f7e5d
developer <17201665342@163.com>
2026-03-22 22:18:00 +0800

    完成离线存储

--- RAG_Project/config_data.py
+++ RAG_Project/config_data.py
@@ -11,3 +11,9 @@ chunk_size = 1000
 chunk_overlap = 100
 separators = ["\n\n", "\n", ".", "！", "?", "。", "!", "?", " ", ""]
 max_split_char_number = 1000        # 文本分割的阈值
+
+#
+similarity_threshold = 1    # 检索返回匹配度的数量
+
+embedding_model_name = "text-embedding-v4"
+chat_model_name = "qwen3-max"
