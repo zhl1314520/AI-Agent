75f8a570937c18065cf1c7e02895e21c65046584
developer <17201665342@163.com>
2026-03-26 19:14:40 +0800

    RAG_Project 支持多文件上传优化

--- RAG_Project/app_file_uploader.py
+++ RAG_Project/app_file_uploader.py
@@ -15,7 +15,7 @@ streamlit.title("知识库更新服务")
 uploader_file = streamlit.file_uploader(
     label="请上传txt文件",
     type=['txt'],
-    accept_multiple_files=False,  # False表示仅接受一个文件的上传
+    accept_multiple_files=True,  # 允许多文件上传
 )
 
 # session_state 是一个字典
@@ -23,19 +23,18 @@ if "service" not in streamlit.session_state:
     service = KnowledgeBaseService()
     streamlit.session_state.service = service
 
-if uploader_file is not None:
-    # 提取文件的信息
-    file_name = uploader_file.name
-    file_type = uploader_file.type
-    file_size = uploader_file.size / 1024  # KB
+if uploader_file:
+    for file in uploader_file:
+        file_name = file.name
+        file_type = file.type
+        file_size = file.size / 1024
 
-    streamlit.subheader(f"文件名：{file_name}")
-    streamlit.write(f"格式：{file_type} | 大小：{file_size:.2f} KB")
+        streamlit.subheader(f"文件名：{file_name}")
+        streamlit.write(f"格式：{file_type} | 大小：{file_size:.2f} KB")
 
-    # get_value -> bytes -> decode('utf-8')
-    text = uploader_file.getvalue().decode("utf-8")
+        text = file.getvalue().decode("utf-8")
 
-    with streamlit.spinner("正在处理..."):          # 显示一个加载动画
-        time.sleep(1)                           # 模拟处理时间 1s
-        result = streamlit.session_state["service"].upload_by_str(text, file_name)
-        streamlit.write(result)
+        with streamlit.spinner("正在处理..."):
+            time.sleep(1)
+            result = streamlit.session_state["service"].upload_by_str(text, file_name)
+            streamlit.write(result)
